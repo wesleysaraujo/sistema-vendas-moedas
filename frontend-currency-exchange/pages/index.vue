dd9568560165a2f2eba2eb877b98eb48c1116ba1
Wesley Serafim <wesleyserafim@MacBook-Pro-de-Wesley.local>
2025-04-17 10:58:57 -0300

    Ajuste da listagem de moedas

--- frontend-currency-exchange/pages/index.vue
+++ frontend-currency-exchange/pages/index.vue
@@ -3,8 +3,7 @@
     <div class="bg-white rounded-lg shadow-md p-6 mb-8">
       <h2 class="text-2xl font-bold mb-6 text-center text-gray-800">Listagem de Moedas</h2>
       
-      <div class="grid grid-cols-1 md:grid-cols-2 content-center gap-6">
-        <div class="overflow-x-auto">
+      <div class="overflow-x-auto">
           <!-- Loading State -->
           <div v-if="loading" class="flex justify-center items-center h-64">
             <svg class="animate-spin h-10 w-10 text-blue-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
@@ -12,21 +11,21 @@
               <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 1 1 16 0A8 8 0 0 1 4 12zm2.5 0a5.5 5.5 0 1 0 11 0A5.5 5.5 0 0 0 6.5 12z"></path>
             </svg>
           </div>
-          <table class="min-w-full bg-white border border-gray-200" v-if="!loading">
-              <thead>
+          <table class="min-w-full divide-y divide-gray-200 bg-white border border-gray-200" v-if="!loading">
+              <thead class="bg-gray-50">
                 <tr class="bg-gray-200 text-gray-600 uppercase text-sm leading-normal p-2">
-                  <th>Código</th>
-                  <th>Nome</th>
-                  <th>Taxa</th>
-                  <th>Ação</th>
+                  <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Código</th>
+                  <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Nome</th>
+                  <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Taxa</th>
+                  <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Ação</th>
                 </tr>
               </thead>
-              <tbody>
+              <tbody class="bg-white divide-y divide-gray-200">
                   <tr v-for="currency in currencies" :key="currency.code" class="border-b border-gray-200 p-6 hover:bg-gray-100">
-                      <td>{{ currency.code }}</td>
-                      <td>{{ currency.name }}</td>
-                      <td>{{ parseFloat(currency.exchange_rate).toFixed(2) }}</td>
-                      <td>
+                      <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{{ currency.code }}</td>
+                      <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{{ currency.name }}</td>
+                      <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{{ parseFloat(currency.exchange_rate).toFixed(2) }}</td>
+                      <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
                         <!-- Botão para comprar com link para Buy passando o parametro code -->
                         <NuxtLink :to="{ name: 'buy', query: { code: currency.code } }" class="bg-blue-500 text-white p-1 rounded hover:bg-blue-600">
                           Comprar  
@@ -36,7 +35,6 @@
               </tbody>
           </table>
         </div>
-      </div>
     </div>
   </div>
 </template>
